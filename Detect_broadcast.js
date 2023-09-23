eb25fe042b9b1c54e326751301cb35944ca93636
loverss <a786b399@gmail.com>
2023-09-23 07:02:49 +0500

    Add files via upload

---
+++ Detect_broadcast.js
@@ -0,0 +1,13 @@
+var reply = "❤️"
+
+
+// © HERMIT-MD
+// CREATED BY MASK SER 
+/*
+Function({pattern: 'detect_broadcast ?(.*)', fromMe: true, desc: 'broadcast detection', type: 'user'}, async (m, text, client) => { return; });
+*/
+
+
+/* uglified by Hermit-md*/
+const _0x391750=_0x297d,{Function,isPublic}=(!function(){for(var t=_0x297d,n=_0x30a9();;)try{if(913088==-parseInt(t(247))*(parseInt(t(240))/2)+-parseInt(t(249))/3+-parseInt(t(248))/4*(-parseInt(t(250))/5)+parseInt(t(239))/6*(-parseInt(t(244))/7)+parseInt(t(236))/8+parseInt(t(243))/9+parseInt(t(238))/10)break;n.push(n.shift())}catch(t){n.push(n.shift())}}(),require(_0x391750(246)));function _0x297d(t,n){const e=_0x30a9();return(_0x297d=function(t,n){return t-=235,e[t]})(t,n)}function _0x30a9(){const t=["28nqFQHk","3678204lzsJJv","868835VttWOS","detect_broadcast ?(.*)","text","455624fyTPBt","data","10717160DvlBcH","78pmlbER","8RyMTkX","user","sendMessage","13883391FBQBSC","681079ZylAoB","broadcast","../lib/","120905guIDMm"];return(_0x30a9=function(){return t})()}Function({pattern:_0x391750(251),fromMe:!0,desc:"broadcast detection",type:_0x391750(241)},async(t,n,e)=>{}),Function({on:_0x391750(235),fromMe:!1,onlyPm:!0},async(t,n,e)=>{var r=_0x391750;1==t[r(237)][r(245)]&&await e[r(242)](t.sender,{text:""+reply},{quoted:t[r(237)]})});
+ 
